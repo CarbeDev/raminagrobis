4f2cce2c521a9bfc82d073a601580c1847834747
Kevin Carbeti <kevin.carbeti@octo.com>
2023-02-25 12:19:34 +0100

    Creation user

---
+++ src/main/kotlin/com/raminagrobis/centraleachat/domain/user/model/Role.kt
@@ -0,0 +1,5 @@
+package com.raminagrobis.centraleachat.domain.user.model
+
+enum class Role{
+    ADMIN,FOURNISSEUR,ADHERENT
+}
---
+++ src/main/kotlin/com/raminagrobis/centraleachat/domain/user/model/User.kt
@@ -0,0 +1,3 @@
+package com.raminagrobis.centraleachat.domain.user.model
+
+class User (val id : Int, var email : String, var prenom : String, var nom : String, val role: Role)
