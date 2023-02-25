f95466988961970c4cef5befd1edfdb23b764bd8
Kevin Carbeti <kevin.carbeti@octo.com>
2023-02-25 09:57:56 +0100

    First commit

---
+++ src/main/kotlin/com/raminagrobis/centraleachat/CentraleAchatApplication.kt
@@ -0,0 +1,11 @@
+package com.raminagrobis.centraleachat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CentraleAchatApplication
+
+fun main(args: Array<String>) {
+    runApplication<CentraleAchatApplication>(*args)
+}
