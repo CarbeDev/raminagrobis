e65b14276c6bafd62135140235b406675327e7e1
Kevin Carbeti <kevin.carbeti@octo.com>
2023-03-03 15:51:19 +0100

    Test DeleteUser

--- src/main/kotlin/com/raminagrobis/centraleachat/domain/user/usecase/DeleteUser.kt
+++ src/main/kotlin/com/raminagrobis/centraleachat/domain/user/usecase/DeleteUser.kt
@@ -2,11 +2,17 @@ package com.raminagrobis.centraleachat.domain.user.usecase
 
 import com.raminagrobis.centraleachat.domain.user.adapter.UserRepoInterface
 import com.raminagrobis.centraleachat.domain.user.model.User
+import org.springframework.stereotype.Service
 
+@Service
 class DeleteUser(val userRepo : UserRepoInterface){
 
-    fun deleteUser(user : User){
-        if(userRepo.getNbCommandeByUser(user) >= 1)  desactiveUser(user) else userRepo.deleteUser(user)
+    fun handle(user : User){
+        if(userRepo.getNbCommandeByUser(user) >= 1) {
+            desactiveUser(user)
+        } else {
+            userRepo.deleteUser(user)
+        }
     }
 
     private fun desactiveUser(user: User) {
