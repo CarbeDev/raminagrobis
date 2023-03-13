8b4f8895f3e78ca9c5ea5aa228175ba19fb69b3b
Kevin Carbeti <kevin.carbeti@octo.com>
2023-03-13 11:46:12 +0100

    ajout postgreSQL et JPA

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-web:3.0.3")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.3")
+    implementation("org.postgresql:postgresql:42.5.4")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
 }
