4dbd0042320b5b93448ffd15f2660746ab983e26
Kevin Carbeti <kevin.carbeti@octo.com>
2023-03-22 14:36:00 +0100

    suppression @Lazy dans usecase

--- src/main/kotlin/com/raminagrobis/centraleachat/domain/administration/usecase/GetSocietes.kt
+++ src/main/kotlin/com/raminagrobis/centraleachat/domain/administration/usecase/GetSocietes.kt
@@ -2,11 +2,10 @@ package com.raminagrobis.centraleachat.domain.administration.usecase
 
 import com.raminagrobis.centraleachat.domain.administration.adapter.ISocieteRepo
 import com.raminagrobis.centraleachat.domain.administration.model.Societe
-import org.springframework.context.annotation.Lazy
 import org.springframework.stereotype.Service
 
 @Service
-class GetSocietes(@Lazy val userRepo: ISocieteRepo) {
+class GetSocietes(private val userRepo: ISocieteRepo) {
 
     fun handle(): Iterable<Societe> {
         return userRepo.getAll()
--- src/main/kotlin/com/raminagrobis/centraleachat/domain/administration/usecase/SuppressionSociete.kt
+++ src/main/kotlin/com/raminagrobis/centraleachat/domain/administration/usecase/SuppressionSociete.kt
@@ -3,11 +3,10 @@ package com.raminagrobis.centraleachat.domain.administration.usecase
 import com.raminagrobis.centraleachat.domain.administration.adapter.ISocieteRepo
 import com.raminagrobis.centraleachat.domain.administration.exception.EmailAlreadyUseException
 import com.raminagrobis.centraleachat.domain.administration.model.Societe
-import org.springframework.context.annotation.Lazy
 import org.springframework.stereotype.Service
 
 @Service
-class SuppressionSociete(@Lazy val userRepo : ISocieteRepo){
+class SuppressionSociete(private val userRepo : ISocieteRepo){
 
     fun handle(societe : Societe){
         if(userRepo.getNbCommandeBySociete(societe) >= 1) {
