db8757bca3c835dd861f1e38e130ef91fc683fdd
Kevin Carbeti <kevin.carbeti@octo.com>
2023-03-28 10:59:08 +0200

    hotfix : dossier back manquant

---
+++ Back/src/main/kotlin/com/raminagrobis/centraleachat/infra/SocieteRepo.kt
@@ -0,0 +1,32 @@
+package com.raminagrobis.centraleachat.infra
+
+import com.raminagrobis.centraleachat.domain.administration.adapter.ISocieteRepo
+import com.raminagrobis.centraleachat.domain.administration.model.Societe
+import org.springframework.stereotype.Repository
+
+@Repository
+class SocieteRepo(private val repo: SQLSociete) : ISocieteRepo {
+    override fun getAll(): Iterable<Societe> {
+        return repo.findAll()
+    }
+
+    override fun saveSociete(societe: Societe) {
+        repo.save(societe)
+    }
+
+    override fun isEmailUnique(email: String): Boolean {
+        return repo.existsByEmail(email)
+    }
+
+    override fun findSocieteByID(id: Int): Societe {
+        return repo.findById(id).orElseThrow()
+    }
+
+    override fun getNbCommandeBySociete(societe: Societe): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteSociete(societe: Societe) {
+        repo.delete(societe)
+    }
+}
---
+++ Back/src/test/kotlin/com/raminagrobis/centraleachat/CentraleAchatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.raminagrobis.centraleachat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CentraleAchatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
