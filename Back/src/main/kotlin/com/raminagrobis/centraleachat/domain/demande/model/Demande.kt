a00a45ca3618f0f321c41afc3a1a099094107624
Kevin Carbeti <kevin.carbeti@octo.com>
2023-05-30 17:07:08 +0200

    refacto demande

--- Back/src/main/kotlin/com/raminagrobis/centraleachat/domain/demande/model/Demande.kt
+++ Back/src/main/kotlin/com/raminagrobis/centraleachat/domain/demande/model/Demande.kt
@@ -1,22 +1,24 @@
-package com.raminagrobis.centraleachat.controller.demande.model
+package com.raminagrobis.centraleachat.domain.demande.model
 
 import com.raminagrobis.centraleachat.domain.administration.model.Categorie
 import com.raminagrobis.centraleachat.domain.administration.model.Societe
-import jakarta.persistence.Column
+import jakarta.persistence.*
 
+@Entity
 class Demande(
+    @Id
     @Column(name = "id_demande")
     var id : Int = 0,
-    @Column(name = "id_societe")
+    @ManyToOne
+    @JoinColumn(name = "id_societe")
     var societe : Societe? = null,
-    @Column(name = "nom_produit_demande")
     var nom : String = "",
-    @Column(name = "description_produit_demande")
     var description : String = "",
-    @Column(name = "id_categorie_produit_demande")
+    @ManyToOne
+    @JoinColumn(name = "id_categorie")
     var categorie: Categorie? = null,
     @Column(name = "etat_demande")
-    var etat : EtatDemande? = null
+    var etat : EtatDemande? = EtatDemande.EN_COURS
 )
 
 enum class EtatDemande{
